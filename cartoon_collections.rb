03e8b5086a31fb30cbd4774d1687e3d7cf6ef30c
Peter Werner <pcw1995@yahoo.com>
2019-09-09 20:38:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(planeteer_calls)
     x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  
 end
 
 def find_the_cheese# code an argument here
