a6092c8f85654b1b6983fe0f5aed5cd0d8921671
Peter Werner <pcw1995@yahoo.com>
2019-09-09 20:50:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,6 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     return cheese if array.include?(cheese)
+    else return nil 
   end
 end
